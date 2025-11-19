87313d8a9712be3d2f5bac97fc168aafcae879a6
Domin122 <dgabrysz3@gmail.com>
2025-11-19 15:02:43 +0100

    commit

--- zwierzeta/app/src/main/java/com/example/zwierzeta/MainActivity.kt
+++ zwierzeta/app/src/main/java/com/example/zwierzeta/MainActivity.kt
@@ -1,18 +1,14 @@
 package com.example.zwierzeta
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ListView
-import android.widget.Spinner
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
         val listView: ListView = findViewById(R.id.ListView)
         val spinner: Spinner = findViewById(R.id.spinner)
@@ -20,25 +16,68 @@ class MainActivity : AppCompatActivity() {
         val editDescipt: EditText = findViewById(R.id.editTextText)
         val button: Button = findViewById(R.id.button)
 
-        val images = listOf(
-            R.drawable.bird,
-            R.drawable.cat,
-            R.drawable.dog
+        val animals = listOf("bird", "cat", "dog","fish","fox","frog","horse","lion","panda")
+
+        spinner.adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_dropdown_item,
+            animals
         )
 
-        val titles = listOf(
-            getString(R.string.title_bird),
-            getString(R.string.title_cat),
-            getString(R.string.title_dog)
+        val animalImages = mapOf(
+            "bird" to R.drawable.bird,
+            "cat" to R.drawable.cat,
+            "dog" to R.drawable.dog,
+            "fish" to R.drawable.fish,
+            "fox" to R.drawable.fox,
+            "frog" to R.drawable.frog,
+            "horse" to R.drawable.horse,
+            "lion" to R.drawable.lion,
+            "panda" to R.drawable.panda
         )
 
-        val descriptions = listOf(
-            getString(R.string.descript_bird),
-            getString(R.string.descript_cat),
-            getString(R.string.descript_dog)
+        val animalTitles = mapOf(
+            "bird" to R.string.title_bird,
+            "cat" to R.string.title_cat,
+            "dog" to R.string.title_dog
         )
 
-        listView.adapter = ItemAdapter(this, images,titles,descriptions)
+        val animalDescriptions = mapOf(
+            "bird" to R.string.descript_bird,
+            "cat" to R.string.descript_cat,
+            "dog" to R.string.descript_dog
+        )
+
+        val images = mutableListOf<Int>()
+        val titles = mutableListOf<String>()
+        val descriptions = mutableListOf<String>()
+
+        val adapter = ItemAdapter(this, images, titles, descriptions)
+        listView.adapter = adapter
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val selected = animals[position]
+                editTitle.setText(getString(animalTitles[selected]!!))
+                editDescipt.setText(getString(animalDescriptions[selected]!!))
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {}
+        }
+
+        button.setOnClickListener {
+            val selected = spinner.selectedItem.toString()
+
+            images.add(animalImages[selected]!!)
+            titles.add(editTitle.text.toString())
+            descriptions.add(editDescipt.text.toString())
+
+            adapter.notifyDataSetChanged()
+        }
     }
-
 }
